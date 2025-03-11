f58607e6003fe9a20f32896e81fb64b829256343
Dayana <dayana.myk@transport4.com>
2025-03-11 14:47:32 -0400

    Interim commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,128 @@
 
 import React from 'react';
 import { languages } from './languages.js';
+import { clsx } from 'clsx';
 import {
   useState,
   useEffect
 
 } from "react";
 
+import { getFarewellText } from './utils.js';
 
 function App() {
-
+  // State values 
   const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
+
+  // Derived values
+  const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length;
   const wordArray = currentWord.split('');
-  const letterElements = wordArray.map((letter, index) => (
-    <span key={index} >{letter.toUpperCase()}</span>
-  ))
 
+  const isGameWon = wordArray.every(letter => guessedLetters.includes(letter));
+  const isGameLost = wrongGuessCount >= languages.length - 1;
+  const isGameOver = isGameWon || isGameLost;
+
+  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
+  const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter);
+  console.log(isLastGuessIncorrect);
+
+
+  //Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const alphabetArray = alphabet.split('');
-  const keyBoardElements = alphabetArray.map((letter, index) => (
-    <button key={index} >{letter.toUpperCase()}</button>
-  ))
 
-  const languagesElements = languages.map(language => {
+
+  //Render keyboard
+  const keyBoardElements = alphabetArray.map((letter, index) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = wordArray.includes(letter) && isGuessed;
+    const isWrong = !wordArray.includes(letter) && isGuessed;
+    const className = clsx({ 'guessed': isGuessed, 'correct': isCorrect, 'wrong': isWrong });
+
+    return (
+      <button key={index}
+        className={className}
+        disabled={isGameOver}
+        onClick={() => addGuessedLetter(letter)} >
+        {letter.toUpperCase()}
+      </button>
+    )
+  })
+
+  //Render guess letters
+  const letterElements = wordArray.map((letter, index) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const displayLetter = isGuessed ? letter.toLocaleUpperCase() : '';
+    return (
+      <span key={index} >{displayLetter}</span>
+    )
+  })
+
+
+  //Render languages
+
+  const languagesElements = languages.map((language, index) => {
 
     const style = { backgroundColor: language.backgroundColor, color: language.color }
+    const isLanguageLost = index < wrongGuessCount
+    const className = clsx("language-box", isLanguageLost && "lost")
     return (
       <span
         key={language.name}
         style={style}
-        className='language-box'
+        className={className}
       >
         {language.name}
       </span>
     )
   })
+
+  //Functions
+  function addGuessedLetter(letter) {
+    console.log(letter);
+    setGuessedLetters(prevLetters => prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]);
+
+
+
+  }
+
+  const gameStatus = clsx("game-status", {
+    "game-lost": isGameLost,
+    "game-won": isGameWon,
+    "farewell": !isGameOver && isLastGuessIncorrect
+  }
+  )
+
+  function renderGameStatus() {
+
+    if (!isGameOver && isLastGuessIncorrect) {
+
+      return (
+        <p> {getFarewellText(languages[wrongGuessCount - 1].name)}</p>)
+    }
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You Win!</h2>
+          <p >Well done! 🎉</p>
+        </>)
+    }
+    if (isGameLost) {
+
+      return (
+        <>
+          <h2>Game Over!!</h2>
+          <p >You lose! Better start learning Assembly.</p>
+        </>)
+
+
+    }
+    return null;
+
+  }
 
 
   return (
@@ -44,9 +131,9 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word withing 8 attempts to keep the programming world safe from Assembly! </p>
       </header>
-      <section className="game-status">
-        <h2>You Win!</h2>
-        <p className="game-status-text">Well done! 🎉</p>
+      <section className={gameStatus}>
+        {renderGameStatus()}
+
       </section>
       <section className="game-languages">
         {languagesElements}
@@ -57,6 +144,7 @@ function App() {
       <section className="keyboard">
         {keyBoardElements}
       </section>
+      {isGameOver && <button className="new-game">New Game</button>}
     </main>
   )
 }
